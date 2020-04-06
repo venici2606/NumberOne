6006eaa2c075fcf33e7383d6b94e70e320caedff
martinlexberg <martin.a.lexberg@hotmail.com>
2020-04-06 14:54:06 +0200

    fix

--- eksamen/src/Exercise.java
+++ eksamen/src/Exercise.java
@@ -1,13 +1,3 @@
 public class Exercise {
-    public static class EnduranceExercise {
-    }
 
-    public static class BalanceExercise {
-    }
-
-    public static class FlexibilityExercise {
-    }
-
-    public static class StrengthExercise {
-    }
 }
