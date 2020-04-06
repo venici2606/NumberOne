2020d870387d95b438de7fee1c0c5b20364f1a72
martinlexberg <martin.a.lexberg@hotmail.com>
2020-04-06 13:23:00 +0200

    Lagt exercises inn

--- eksamen/src/Exercise.java
+++ eksamen/src/Exercise.java
@@ -1,2 +1,13 @@
 public class Exercise {
+    public static class EnduranceExercise {
+    }
+
+    public static class BalanceExercise {
+    }
+
+    public static class FlexibilityExercise {
+    }
+
+    public static class StrengthExercise {
+    }
 }
