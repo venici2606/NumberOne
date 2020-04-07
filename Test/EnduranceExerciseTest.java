462c90d2163a06f63377b5e5ef95abae1d92a506
martinlexberg <martin.a.lexberg@hotmail.com>
2020-04-07 19:42:28 +0200

    Testing EnduranceExercise

--- Test/EnduranceExerciseTest.java
+++ Test/EnduranceExerciseTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EnduranceExerciseTest {
+    @Test
+    public void EnduranceClassTest(){
+        Exercise EnduranceExerciseTest  = new EnduranceExercise(1, 2, 3, 4, "Dumbell");
+        assertTrue(EnduranceExerciseTest.intensity ==1);
+        System.out.println("Testing for correct intensity ");
+        assertTrue(EnduranceExerciseTest.duration ==2);
+        System.out.println("Testing for correct duration ");
+        assertTrue(EnduranceExerciseTest.repetitions ==3);
+        System.out.println("Testing for correct repetitions ");
+        assertTrue(EnduranceExerciseTest.sets ==4);
+        System.out.println("Testing for correct sets ");
+        assertTrue(EnduranceExerciseTest.toString().contains("D"));
+        System.out.println("Testing for correct equipment ");
+    }
+    @Test
+    public void testToString() {
+        Exercise EnduranceExercise  = new EnduranceExercise(1, 2, 3, 4, "Dumbell");
+        equals(EnduranceExercise.toString().contains("s"));
+        System.out.println("Testing for Check of String");
+    }
+
 }
+
