2c050af998f91fa2779c4d314431b72c320fe93c
venici2606 <venicia_p@live.no>
2020-04-08 03:02:51 +0200

    lagt til person array test inne på exercise manager

--- Test/ExerciseManagerTest.java
+++ Test/ExerciseManagerTest.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertTrue;
+
 public class ExerciseManagerTest {
+
+    @org.junit.Test
+    public void personsIntoList(){
+        ArrayList<Person> personList = new ArrayList<>();
+
+        Person personTest1 = new Person("body weight", 30);
+        personList.add(personTest1);
+
+        Person personTest2 = new Person("zumba", 40);
+        personList.add(personTest2);
+
+        assertTrue(personList.contains(personTest1));
+        assertTrue(personList.contains(personTest2));
+
+    }
 }
--- Test/PersonTest.java
+++ Test/PersonTest.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-
-
 public class PersonTest {
 
     @org.junit.Test
