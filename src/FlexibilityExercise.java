3dbd50bfe9961c4c1258927bec0880f217b9eb81
1drees <1drees>
2020-04-08 11:36:12 +0200

    Balance og Flexibility

--- src/FlexibilityExercise.java
+++ src/FlexibilityExercise.java
@@ -1,2 +1,17 @@
-public class FlexibilityExercise {
+public class FlexibilityExercise extends Exercise{
+    int requirement;
+
+    public FlexibilityExercise(int intensity, int duration, int repetitions, int sets, int requirement) {
+        super(intensity, duration, repetitions, sets);
+        this.requirement = requirement;
+    }
+
+    @Override
+    public String toString() {
+        return "For this flexibility exercise you need to "+ requirement + '\n' +
+                "Intensity is " + intensity + "% " +'\n' +
+                "Duration is " + duration + " mins "+ '\n' +
+                repetitions + " Repetitions" +'\n' +
+                sets + " Sets " +'\n'+'\n';
+    }
 }
