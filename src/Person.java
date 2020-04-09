32e173997eb2af914f885283cfeb38855ca44690
venici2606 <venicia_p@live.no>
2020-04-09 03:04:10 +0200

    lagt til ekstra instans

--- src/Person.java
+++ src/Person.java
@@ -3,16 +3,40 @@ import java.util.ArrayList;
 public class Person {
     String preferredExercise;
     int acceptableIntensity;
+    static String currentProgram;
 
-    public Person(String preferredExercise, int acceptableIntensity) {
+    public Person(String preferredExercise, int acceptableIntensity, String currentProgram) {
         this.preferredExercise = preferredExercise;
         this.acceptableIntensity = acceptableIntensity;
+        this.currentProgram = currentProgram;
     }
+
+    public String getPreferredExercise() {
+        return preferredExercise;
+    }
+
+    public int getAcceptableIntensity() {
+        return acceptableIntensity;
+    }
+
+    public String getCurrentProgram(){
+        return currentProgram;
+    }
+
+        /*
+    //method overloading
+    public Person(String preferredExercise, int acceptableIntensity, String name) {
+        this.preferredExercise = preferredExercise;
+        this.acceptableIntensity = acceptableIntensity;
+        this.name = name;
+    }
+        */
 
     @Override
     public String toString() {
         return "Preferred exercise: " + preferredExercise + '\n' +
-                "Acceptable intensity: " + acceptableIntensity + " %" + '\n';
+                "Acceptable intensity: " + acceptableIntensity + " %" + '\n' +
+                "Current program: " + currentProgram + '\n';
     }
 /*
     ArrayList<Person> personData = new ArrayList<>();
