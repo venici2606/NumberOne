8c8819280f37e2849eafea87aa92085e8b3f2508
venici2606 <venicia_p@live.no>
2020-04-09 03:04:46 +0200

    laget test til ny input

--- Test/PersonTest.java
+++ Test/PersonTest.java
@@ -1,17 +1,38 @@
+import static org.junit.Assert.assertTrue;
+
 public class PersonTest {
 
     @org.junit.Test
-    public void PersonValue() {
+    public void testPerson() {
+        Person personTest1 = new Person("yoga", 60, "Program en");
+        assertTrue(personTest1.preferredExercise == "yoga");
+        System.out.println("Testing for correct preferred exercise");
 
-        Person personTest = new Person(" ", -1);
+        assertTrue(personTest1.acceptableIntensity == 60);
+        System.out.println("Testing for correct acceptable intensity");
 
-        if((personTest.preferredExercise == "") || (personTest.preferredExercise == " ")) {
-            System.out.println("You need to put valid exercise!");
-        } else if (personTest.acceptableIntensity < 10) {
-            System.out.println("You need to have an intensity level over 10 % ");
-        } else {
-            System.out.println("You have valid values");
-        }
+//        assertTrue(personTest1.preferredExercise == "yoga" && personTest1.acceptableIntensity == 60);
+//        System.out.println("Testing for correct preferred exercise and acceptable intensity");
+
+
+//        if(assertTrue(personTest1.preferredExercise == "yoga" && personTest1.acceptableIntensity == 60)) {
+//            System.out.println("Testing for incorrect preferred exercise");
+//            assertTrue(false); //assertFalse(false);
+//        } else {
+//            System.out.println("Testing for correct preferred exercise");
+//            assertTrue(true);
+//        }
+//            //assertTrue(person1.preferredExercise = "");
+//        assertTrue(personTest1.acceptableIntensity > 10);
+//        System.out.println("Testing for correct acceptable intensity");
     }
 
+
+    @org.junit.Test
+    public void testToString() {
+        Person personTest1  = new Person("yoga", 60, "Program en");
+        equals(personTest1.toString().contains("yoga"));
+        equals(personTest1.toString().contains("60"));
+        System.out.println("Testing for Check of String");
+    }
 }
