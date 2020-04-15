67f518cd111659eaa8c326371f82f0e63c128191
venici2606 <venicia_p@live.no>
2020-04-15 12:51:30 +0200

    Made a program for Person

--- src/ExerciseManager.java
+++ src/ExerciseManager.java
@@ -33,7 +33,7 @@ public class ExerciseManager {
         // (the highest intensity among the included exercises).
 
         p.overallIntensity();
-        System.out.println(p.intensitylevel + " % is the overall intensity for this program" + '\n'); /* hvilket prgram? */
+        System.out.println(p.intensitylevel + " % is the overall intensity for this program" + '\n');
 
 // 1 b. Each program keeps account of whether or not it is balanced.
 //     A balanced program has at least one exercise of each type.
@@ -59,34 +59,40 @@ public class ExerciseManager {
 
         System.out.println(e.toString()); /* skriver ut siste fra arrayet */
 
-        /* TEST */
-        Exercise exercise_e = new EnduranceExercise(50, 30, 15, 3, "yoga");
-        exercises.add(exercise_e);
+// Add program to Person - START
+        Exercise exercise_e1 = new EnduranceExercise(70, 30, 15, 3, "yoga");
+        exercises.add(exercise_e1);
+        Exercise exercise_s1 = new StrengthExercise(60, 20, 8, 3, 5, "kettle bell");
+        exercises.add(exercise_s1);
+        Exercise exercise_b1 = new BalanceExercise(40, 15, 4, 2, "yoga mat");
+        exercises.add(exercise_b1);
+        Exercise exercise_f1 = new FlexibilityExercise(50, 30, 12, 2, "nothing");
+        exercises.add(exercise_f1);
 
         Program program1 = new Program();
-        //program1.addExercises(exercises);
-        program1.exercises.add(exercise_e);
+        program1.exercises.add(exercise_e1);
+        program1.exercises.add(exercise_s1);
+        program1.exercises.add(exercise_b1);
+        program1.exercises.add(exercise_f1);
 
-        Exercise exercise_s = new EnduranceExercise(70, 15,15,6,"running shoes");
-        exercises.add(exercise_s);
+        Exercise exercise_e2 = new EnduranceExercise(70, 15,15,6,"running shoes");
+        exercises.add(exercise_e2);
 
         Program program2 = new Program();
-        //program2.addExercises(exercises);
-        program2.exercises.add(exercise_s);
+        program2.exercises.add(exercise_e2);
 
 
-
-        ArrayList<Person> personList = new ArrayList<>();
+        // ArrayList<Person> personList = new ArrayList<>();
 
         Person person1 = new Person("kettle", 40, program1);
-        personList.add(person1);
+        //personList.add(person1);
 
         Person person2 = new Person("yoga", 60, program2);
-        personList.add(person2);
+        //personList.add(person2);
 
         System.out.println(person1);
         System.out.println(person2);
-        /* TEST SLUTT */
+// Add program to Person - END
 
 /*
         // for-each loop to divide the array
@@ -110,8 +116,8 @@ public class ExerciseManager {
         // System.out.println(p.exercises);
 
     }
-/*
-    public  doesProgramFit(Person, Program) {
+
+    public doesProgramFit(Person, Program) {
         if(Program.exercises.includes(Person.preferredExercises) &&
                 !(Program.intensity > Person.preferredExcercise +10) &&
                 !(Program.intensity > preferredIntensity - 10) {
@@ -119,6 +125,6 @@ public class ExerciseManager {
         } else {
             System.out.println("Feil");
         }
-    }*/
+    }
 
 }
