d87fc103033676bd1a2ef194b095218eeca1fdb2
venici2606 <56083213+venici2606@users.noreply.github.com>
2020-04-22 14:26:54 +0200

    added tests

--- Test/FlexibilityExerciseTest.java
+++ Test/FlexibilityExerciseTest.java
@@ -1,41 +1,31 @@
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FlexibilityExerciseTest {
-    @org.junit.jupiter.api.Test
-    void testToString() {
-        //TODO clean up
+    @org.junit.Test
+    public void flexibilityClassTest(){
+        Exercise FlexibilityExerciseTest  = new FlexibilityExercise(60, 20, 8, 3, "nothing");
 
-        // Lager ArrayList for programmene
-        ArrayList<Exercise> exercises = new ArrayList<>();
+        assertTrue(FlexibilityExerciseTest.intensity == 60);
+        System.out.println("Testing for correct intensity ");
 
+        assertTrue(FlexibilityExerciseTest.duration == 20);
+        System.out.println("Testing for correct duration ");
 
-        // Gjorde alle exercisene hver for seg - tungvindt. Laget heller forkortelser - mindre koding / mer ryddig.
-        // Nå heter alle Exercises e
+        assertTrue(FlexibilityExerciseTest.repetitions == 8);
+        System.out.println("Testing for correct repetitions ");
 
-        Exercise e;
+        assertTrue(FlexibilityExerciseTest.sets == 3);
+        System.out.println("Testing for correct sets ");
 
-        e = new FlexibilityExercise(1,3,2,4, "Nothing");
-        exercises.add(e);
-        e = new FlexibilityExercise(4,3,2,4, "Moderate flexibility");
-        exercises.add(e);
+        assertTrue(((FlexibilityExercise) FlexibilityExerciseTest).requirement.contains("nothing"));
+        System.out.println("Testing for correct equipment ");
+        }
 
-
-        //  Laget en forkortelse for Program for så å lage programmene
-        // Trenger random input for å teste - Bilal
-        Program p;
-        p = new Program();
-
-        //  Her hentes exercisene ut fra arrayet over for så å bli skrevet ut.
-        p.addExercises(exercises);
-        System.out.println(p.exercises);
-
-        System.out.println(e.toString());
-
-        // Slik så det ut før jeg forkortet Exercise til e.
-//        System.out.println(e1.toString());
-//        System.out.println(e2.toString());
-//        System.out.println(e3.toString());
-//        System.out.println(e4.toString());
-    }
+        @org.junit.Test
+        public void testToString() {
+            Exercise  FlexibilityExercise  = new FlexibilityExercise(60, 20, 8, 3, "nothing");
+            equals(FlexibilityExercise.toString().contains("flexibility"));
+            System.out.println("Testing for Check of String");
+        }
 }
 
