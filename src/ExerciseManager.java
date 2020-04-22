6d1d4060313305717fe89f106dd4986aaeb656ec
venici2606 <venicia_p@live.no>
2020-04-22 13:06:21 +0200

    from master and some clean up on person

--- src/ExerciseManager.java
+++ src/ExerciseManager.java
@@ -10,98 +10,6 @@ public class ExerciseManager {
 
         Program p = new Program();
         Program program1 = new Program();
-<<<<<<< HEAD
-        program1.exercises.add(exercise_e1);
-        program1.exercises.add(exercise_s1);
-        program1.exercises.add(exercise_b1);
-        program1.exercises.add(exercise_f1);
-
-
-        Exercise exercise_e2 = new EnduranceExercise(60, 30,0,0,"cycle");
-        exercises.add(exercise_e2);
-        Exercise exercise_f2 = new FlexibilityExercise(50, 30, 12, 2, "nothing");
-        exercises.add(exercise_f2);
-
-        Program program2 = new Program();
-        program2.exercises.add(exercise_e2);
-        program2.exercises.add(exercise_f2);
-
-        Exercise exercise_b3 = new BalanceExercise(50, 25, 8, 2, "yoga mat");
-        exercises.add(exercise_b3);
-        Exercise exercise_s3 = new StrengthExercise(60, 40, 12, 4, 0, "body weight");
-        exercises.add(exercise_s3);
-
-        Program program3 = new Program();
-        program3.exercises.add(exercise_b3);
-        program3.exercises.add(exercise_s3);
-
-
-        // ArrayList<Person> personList = new ArrayList<>();
-
-        Person person1 = new Person("endurance", 61, program1);
-        //personList.add(person1);
-
-        Person person2 = new Person("strength", 60, program2);
-        //personList.add(person2);
-
-        Person person3 = new Person("balance", 50, program1);
-
-        System.out.println(person1);
-        program1.overallIntensity();
-        System.out.println("Overall intensity for this program: " + program1.intensitylevel + " % \n");
-
-        System.out.println(person2);
-        program2.overallIntensity();
-        System.out.println("Overall intensity for this program: " + program2.intensitylevel + " % \n");
-
-// Add program to Person - END
-
-/*
-        // for-each loop to divide the array
-        for (Person list : personList){
-            System.out.println(list);
-        }
-
- */
-
-
-        /*
-        Person pers;
-
-        pers = new Person("kettle", 40);
-        pers = new Person("yoga", 60);
-        pers = new Person("running", 70);
-
-        System.out.println(pers.toString());
-        */
-
-        // System.out.println(p.exercises);
-
-        System.out.println(doesProgramFit(person1, program1));
-
-        System.out.println(program1.overallIntensity());
-        System.out.println(program2.overallIntensity());
-        System.out.println(program3.overallIntensity());
-
-
-    }
-
-     public static boolean doesProgramFit(Person person, Program program) {
-        if(program.exercises.toString().contains(person.preferredExercise) &&
-                (program.overallIntensity() <= (person.acceptableIntensity + 10) ) &&
-                (program.overallIntensity() >= (person.acceptableIntensity - 10) ) ){
-            System.out.print("Does program fit person? ");
-            return true;
-        } else {
-            //System.out.print("Does program fit person? ");
-            return false;
-        }
-
-    }
-
-
-
-=======
         Program program2 = new Program();
         Program program3 = new Program();
         Program program4 = new Program();
@@ -140,6 +48,4 @@ public class ExerciseManager {
         System.out.println( p.giveProgram(person3));
 
     }
->>>>>>> e2bab19da92a665d6ce53dd2ab81cf7aeb7ad930
 }
-
--- src/Program.java
+++ src/Program.java
@@ -1,121 +1,111 @@
-    import java.util.ArrayList;
-    import java.util.Arrays;
-    import java.util.Collections;
-    import java.util.List;
+import java.util.ArrayList;
+
+public class Program {
+    int duration;
+    int intensitylevel;
 
 
-    public class Program {
-        int duration;
-        int intensitylevel;
+    ArrayList<Exercise> exercises = new ArrayList<>();
+    ArrayList <Program> listOfPrograms = new ArrayList<>();
 
+    public void addExercises(ArrayList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+    public void setDuration(int duration) {
+        this.duration += duration;
+    }
 
-        ArrayList<Exercise> exercises = new ArrayList<>();
-        ArrayList <Program> listOfPrograms = new ArrayList<>();
-
-        public void addExercises(ArrayList<Exercise> exercises) {
-            this.exercises = exercises;
-        }
-        public void setDuration(int duration) {
-            this.duration += duration;
-        }
-
-        // Each program keeps an account of the overall intensity (the highest intensity among the included exercises).
-        public int overallIntensity(){
-            intensitylevel = 0;
-            for (Exercise e: exercises){
-                if(e.intensity >intensitylevel){
-                    intensitylevel = e.intensity;
-                }
+    // Each program keeps an account of the overall intensity (the highest intensity among the included exercises).
+    public int overallIntensity(){
+        intensitylevel = 0;
+        for (Exercise e: exercises){
+            if(e.intensity >intensitylevel){
+                intensitylevel = e.intensity;
             }
-            return intensitylevel;
         }
+        return intensitylevel;
+    }
 //   Each program keeps account of whether or not it is balanced.
 //  A balanced program has at least one exercise of each type.
 
-        public boolean isProgramBalanced() {
-            boolean isBalanced = false;
+    public boolean isProgramBalanced() {
+        boolean isBalanced = false;
 
-            if (exercises.size() >= 4) {
-                if (exercises.toString().contains("endurance")) {
-                    if (exercises.toString().contains("flexibility")) {
-                        if (exercises.toString().contains("strength")) {
-                            if (exercises.toString().contains("balance")) {
-                                isBalanced = true;
-                            }
+        if (exercises.size() >= 4) {
+            if (exercises.toString().contains("endurance")) {
+                if (exercises.toString().contains("flexibility")) {
+                    if (exercises.toString().contains("strength")) {
+                        if (exercises.toString().contains("balance")) {
+                            isBalanced = true;
                         }
                     }
                 }
             }
-            return isBalanced;
         }
+        return isBalanced;
+    }
 
 // 1c. Each program has the exercises sorted in order of increasing intensity.
 
 
-        //  Each program has an overall duration.The duration of an exercise( Program) is twice the sum of the
+    //  Each program has an overall duration.The duration of an exercise( Program) is twice the sum of the
 // durations of all the exercises it includes (to account for rest).*/
-        public String calculateDuration() {
-            int totalDuration = 0;
-            for (Exercise e : exercises) {
-                totalDuration += e.duration*2;
-            }
-            return  "This program takes "+totalDuration+ " minutes from start to finish";
+    public String calculateDuration() {
+        int totalDuration = 0;
+        for (Exercise e : exercises) {
+            totalDuration += e.duration*2;
         }
-
-<<<<<<< HEAD
-
-
+        return  "This program takes "+totalDuration+ " minutes from start to finish";
     }
-=======
-        //   Method for building new Programs
-        public void makeProgram(ArrayList<Exercise> exercises, Program p){
-            // Exercises are grouped into Programs.
-            p.addExercises(exercises);
-            p.calculateDuration();
-            p.isProgramBalanced();
-            listOfPrograms.add(p);
 
-            System.out.println('\n'+ "Program: " +'\n'+
-                    p.calculateDuration() + '\n' +
-                    "Is this program balanced: " + p.isProgramBalanced() + '\n' +
-                    "This program has this level of intensity: "+  p.overallIntensity() + '\n' +
-                    "This program contains these exercises: " + '\n' + '\n' +
-                    p.exercises);
-        }
+    //   Method for building new Programs
+    public void makeProgram(ArrayList<Exercise> exercises, Program p){
+        // Exercises are grouped into Programs.
+        p.addExercises(exercises);
+        p.calculateDuration();
+        p.isProgramBalanced();
+        listOfPrograms.add(p);
 
-        //    Methods for deciding if a Program is appropriate for a given Person
-        public boolean doesProgramFit (Person person, Program program) {
-            program.overallIntensity();
-            return program.exercises.toString().contains(person.preferredExercise) && program.overallIntensity() <= (person.acceptableIntensity + 10) &&
-                    program.overallIntensity() >= (person.acceptableIntensity - 10);
-        }
-        //   A method for recommending, from a given collection of Programs, which is most appropriate for a given Person.
-        public String giveProgram( Person person) {
-            int number = 100;
-            int saveNumber = 100;
-            for (int i = 0; i < listOfPrograms.size(); i++) {
-                int pAI = person.acceptableIntensity;
-                int lGO = listOfPrograms.get(i).overallIntensity();
-                if (doesProgramFit(person, listOfPrograms.get(i))) {
-                    for(int i1 =10; i1 > 0; i1--){
-                        if(pAI <= (lGO + i1) && pAI >= (lGO - i1)) {
-                            number = i1;
-                        }
-                    }
-                    if (number < saveNumber) {
-                        saveNumber = number;
-                        person.currentProgram = listOfPrograms.get(i);
+        System.out.println('\n'+ "Program: " +'\n'+
+                p.calculateDuration() + '\n' +
+                "Is this program balanced: " + p.isProgramBalanced() + '\n' +
+                "This program has this level of intensity: "+  p.overallIntensity() + '\n' +
+                "This program contains these exercises: " + '\n' + '\n' +
+                p.exercises);
+    }
+
+    //    Methods for deciding if a Program is appropriate for a given Person
+    public boolean doesProgramFit (Person person, Program program) {
+        program.overallIntensity();
+        return program.exercises.toString().contains(person.preferredExercise) && program.overallIntensity() <= (person.acceptableIntensity + 10) &&
+                program.overallIntensity() >= (person.acceptableIntensity - 10);
+    }
+    //   A method for recommending, from a given collection of Programs, which is most appropriate for a given Person.
+    public String giveProgram( Person person) {
+        int number = 100;
+        int saveNumber = 100;
+        for (int i = 0; i < listOfPrograms.size(); i++) {
+            int pAI = person.acceptableIntensity;
+            int lGO = listOfPrograms.get(i).overallIntensity();
+            if (doesProgramFit(person, listOfPrograms.get(i))) {
+                for(int i1 =10; i1 > 0; i1--){
+                    if(pAI <= (lGO + i1) && pAI >= (lGO - i1)) {
+                        number = i1;
                     }
                 }
-            }
-            if(person.currentProgram.overallIntensity() == 0){
-                return "We are so sorry, we could not find a appropriate program for you";
-            }else {
-                return "We have assigned this program to you:  "+ '\n' +
-                        "The programs overall intensity is "+ person.currentProgram.overallIntensity() + " and this is your preferred intensity " + person.acceptableIntensity + '\n'+
-                        person.currentProgram.exercises;
+                if (number < saveNumber) {
+                    saveNumber = number;
+                    person.currentProgram = listOfPrograms.get(i);
+                }
             }
         }
->>>>>>> e2bab19da92a665d6ce53dd2ab81cf7aeb7ad930
-
+        if(person.currentProgram.overallIntensity() == 0){
+            return "We are so sorry, we could not find a appropriate program for you";
+        }else {
+            return "We have assigned this program to you:  "+ '\n' +
+                    "The programs overall intensity is "+ person.currentProgram.overallIntensity() + " and this is your preferred intensity " + person.acceptableIntensity + '\n'+
+                    person.currentProgram.exercises;
+        }
     }
+
+}
