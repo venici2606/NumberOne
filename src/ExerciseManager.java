229f8dd53c747337efd52acdf6dbffc22551518f
bilalhaider1 <56349952+bilalhaider1@users.noreply.github.com>
2020-04-24 11:44:59 +0200

    Sorted exercises after intensitive.

--- src/ExerciseManager.java
+++ src/ExerciseManager.java
@@ -19,6 +19,7 @@ public class ExerciseManager {
         exercises1.add(new EnduranceExercise(50, 30, 4, 1, "Stop watch"));
         exercises1.add(new FlexibilityExercise(40, 10, 1, 1, "A yoga mat"));
         exercises1.add(new BalanceExercise(40, 15, 3, 2, "A Balance ball"));
+        p.sortExercise(exercises1);
 //makeProgram is referred to a method in Program-class, that takes the exercises we added and makes it into a program
         p.makeProgram(exercises1, program1);
 
@@ -26,6 +27,7 @@ public class ExerciseManager {
         exercises2.add(new FlexibilityExercise(25, 10, 2, 2, "A yoga mat and being able to sit in a lotus position"));
         exercises2.add(new BalanceExercise(15, 5, 15, 2, "A tree"));
         exercises2.add(new BalanceExercise(20, 5, 15, 2, "A climbing wall"));
+        p.sortExercise(exercises2);
         p.makeProgram(exercises2, program2);
 
 //Creating exercises to add to another program
@@ -34,10 +36,13 @@ public class ExerciseManager {
         exercises3.add(new FlexibilityExercise(30, 6, 6, 5, "Needs to be able to do a split"));
         exercises3.add(new FlexibilityExercise(15, 15, 1, 1, "No requirements"));
         exercises3.add(new BalanceExercise(45, 8, 20, 2, "Bosu ball"));
+        p.sortExercise(exercises3);
         p.makeProgram(exercises3, program3);
 
         exercises4.add(new FlexibilityExercise(50,30,5,6,"Needs to know basic yoga"));
+        p.sortExercise(exercises4);
         p.makeProgram(exercises4, program4);
+        
 
         Person person1 = new Person("endurance", 55, p);
         Person person2 = new Person("strength", 80, p);
