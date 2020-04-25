08b8f73718f82a05a8210395692a0b8ea2b695e2
venici2606 <venicia_p@live.no>
2020-04-25 23:45:27 +0200

    added all the the classes

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,48 +1,37 @@
 public abstract class Exercise implements Comparable<Exercise> {
-        int intensity;
-        int duration;
-        int repetitions;
-        int sets;
+    int intensity;
+    int duration;
+    int repetitions;
+    int sets;
 
-        public Exercise(int intensity, int duration, int repetitions, int sets) {
-<<<<<<< HEAD
-
-            if (intensity <= 0 || intensity >= 100) {
-                throw new IllegalArgumentException("Invalid intensity value");
-            }
-
-
-            // the intensity can't be negative, 0, 100 or higher than 100
-=======
+    public Exercise(int intensity, int duration, int repetitions, int sets) {
 // the intensity can't be negative, 0, 100 or higher than 100
->>>>>>> 05fcda55c3377f515547b53b2dfe7fe6eb7ac551
-            if (intensity <= 0 || intensity >= 100) {
-                    throw new IllegalArgumentException("Invalid intensity value");
-            }
-
-            this.intensity = intensity;
-            this.duration = duration;
-            this.repetitions = repetitions;
-            this.sets = sets;
+        if (intensity <= 0 || intensity >= 100) {
+            throw new IllegalArgumentException("Invalid intensity value");
         }
 
-        @Override
-        public String toString() {
-            return "exercise \n" +
-                    "Intensity is " + intensity + "% " +'\n' +
-                    "Duration is " + duration + " mins "+ '\n' +
-                    repetitions + " Repetitions" +'\n' +
-                    sets + " Sets " +'\n'+'\n';
-        }
+        this.intensity = intensity;
+        this.duration = duration;
+        this.repetitions = repetitions;
+        this.sets = sets;
+    }
 
-        public int getIntensity(){
-            return intensity;
-        }
+    @Override
+    public String toString() {
+        return "exercise " + '\n' +
+                "Intensity is " + intensity + "% " +'\n' +
+                "Duration is " + duration + " mins "+ '\n' +
+                repetitions + " Repetitions" +'\n' +
+                sets + " Sets " +'\n'+'\n';
+    }
 
-        @Override
-        public int compareTo(Exercise o) {
-            return this.getIntensity()-o.getIntensity();
-        }
+    public int getIntensity(){
+        return intensity;
+    }
+
+    @Override
+    public int compareTo(Exercise o) {
+        return this.getIntensity()-o.getIntensity();
+    }
 }
-
 
--- src/Person.java
+++ src/Person.java
@@ -9,18 +9,10 @@ public class Person {
         this.currentProgram = currentProgram;
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 05fcda55c3377f515547b53b2dfe7fe6eb7ac551
     @Override
     public String toString() {
         return "Preferred exercise: " + preferredExercise + '\n' +
                 "Acceptable intensity: " + acceptableIntensity + " %" + '\n' +
                 "Current program: " + currentProgram.exercises + '\n';
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 05fcda55c3377f515547b53b2dfe7fe6eb7ac551
 }
